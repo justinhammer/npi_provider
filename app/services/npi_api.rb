03c5b0a4ecb6ee5c5e7cd2668ba91115a6d37d03
Justin Hammer <justhamr@gmail.com>
2022-04-05 20:54:54 -0600

    Add API service

---
+++ app/services/npi_api.rb
@@ -0,0 +1,19 @@
+# app/services/npi_api.rb
+
+class NpiApi < ApplicationRecord
+  BASE_URL = 'https://npiregistry.cms.hhs.gov/api/'.freeze
+
+  def self.npi_send(method, query: {})
+    url = BASE_URL
+    json_response = nil
+
+    response = HTTParty.public_send(method, url, query: query)
+
+    begin
+      json_response = JSON.parse(response.body, symbolize_names: true) || {}
+    rescue StandardError
+      raise "Failed to connect to NPI Registry at #{url}"
+    end
+    [response_code: response.code, json_response: json_response]
+  end
+end
