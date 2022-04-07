695b47b0e87915acd3cc0828891c8a632781677d
Justin Hammer <justhamr@gmail.com>
2022-04-07 01:16:05 -0600

    Get create working with messages

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,6 +1,31 @@
 # app/controllers/providers_controller.rb
 
 class ProvidersController < ApplicationController
+  include ProviderConcern
+
   def index
+    @providers = Provider.all
+    @provider = @providers.find(params[:id]) if @providers.present? && params[:id].present?
+  end
+
+  def create
+    api_response = send_npi_request(provider_params&.dig(:external_id))
+    if api_response.dig(:results).empty?
+      flash[:notice] = "No Provider found with that NPI number"
+      return redirect_to action: 'index'
+    end
+
+    @provider = Provider.new(external_id: api_response.dig(:results).first.dig(:number), details: api_response.dig(:results))
+    if @provider.save
+      flash[:notice] = "Provider found!"
+      redirect_to providers_path(id: @provider.id)
+    else
+      flash[:notice] = "Unable to save Provider: #{@provider.errors.full_messages}"
+      return redirect_to action: 'index'
+    end
+  end
+
+  def provider_params
+    params.permit(:external_id)
   end
 end
