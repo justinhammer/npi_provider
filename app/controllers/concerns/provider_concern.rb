d1be15720f090f8b8fd813a284142da191896783
Justin Hammer <justhamr@gmail.com>
2022-04-06 23:59:43 -0600

    Make it so controller doesn't need to know about api version number

--- app/controllers/concerns/provider_concern.rb
+++ app/controllers/concerns/provider_concern.rb
@@ -3,8 +3,8 @@
 module ProviderConcern
   extend ActiveSupport::Concern
 
-  def send_npi_request(version, external_id)
-    json_response = NpiApi.npi_send('get', query: {'version' => version, 'number' => external_id})
+  def send_npi_request(external_id)
+    json_response = NpiApi.npi_send('get', query: {'number' => external_id})
     return json_response if json_response.dig(:Errors).blank?
 
     raise ApiError.new("An API Error has occurred", json_response.dig(:Errors))
--- app/services/npi_api.rb
+++ app/services/npi_api.rb
@@ -2,12 +2,13 @@
 
 class NpiApi
   BASE_URL = 'https://npiregistry.cms.hhs.gov/api/'.freeze
+  API_VERSION = '2.1'.freeze
 
   def self.npi_send(method, query: {})
     url = BASE_URL
     json_response = nil
 
-    response = HTTParty.public_send(method, url, query: query)
+    response = HTTParty.public_send(method, url, query: query.merge('version': API_VERSION))
 
     begin
       json_response = JSON.parse(response.body, symbolize_names: true) || {}
