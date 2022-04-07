ec405cf37b0c740f205a349af55d88c8fde84254
Justin Hammer <justhamr@gmail.com>
2022-04-07 04:18:43 -0600

    remove extra query

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -5,7 +5,7 @@ class ProvidersController < ApplicationController
 
   def index
     @providers = Provider.all
-    @provider = @providers.find(params[:id]) if @providers.present? && params[:id].present?
+    @provider = @providers.where(id: params[:id]) if @providers.present? && params[:id].present?
     @providers = @providers.sort_by { |provider| [ provider == @provider ? 0 : 1, provider ]  }
   end
 
